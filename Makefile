28cff0c3c6e952d1f06ce861e62d9c013427a6ba
Jacob Kalat <kalatjacob@gmail.com>
2021-04-17 18:56:15 -0400

    prints interfaces

---
+++ Makefile
@@ -0,0 +1,14 @@
+SHELL := /bin/bash
+CC=g++
+CFLAGS=-pedantic -ansi -Wall -lpcap
+
+all: netCanvas
+	
+netCanvas: main.cpp
+	$(CC) $(CFLAGS) -g -o netCanvas main.cpp
+
+valgrind: maze
+	valgrind --tool=memcheck --leak-check=yes ./netCanvas
+
+clean:
+	rm netCanvas
---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <pcap.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	pcap_if_t *alldevs;
+	pcap_if_t *d;
+	int i=0;
+	char errbuf[PCAP_ERRBUF_SIZE];
+
+	if (pcap_findalldevs(&alldevs, errbuf) == -1)
+	{
+		fprintf(stderr,"Error in pcap_findalldevs_ex: %s\n", errbuf);
+		exit(1);
+	}
+
+	for(d= alldevs; d != NULL; d= d->next)
+	{
+		printf("%d. %s", ++i, d->name);
+		if (d->description)
+			printf(" (%s)\n", d->description);
+		else
+			printf(" (No description available)\n");
+	}
+	
+	return(0);
+}
