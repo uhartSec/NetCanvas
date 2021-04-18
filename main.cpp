1e1191f397a78fec6371d54f034dcc952c21ff3a
Jacob Kalat <kalatjacob@gmail.com>
2021-04-18 16:47:55 -0400

    please

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <pcap.h>
 #include <stdlib.h>
+#include <arpa/inet.h>
+void ifprint(pcap_if_t *d);
 
 int main(int argc, char *argv[])
 {
 	pcap_if_t *alldevs;
 	pcap_if_t *d;
 	int i=0;
+	int inum;
 	char errbuf[PCAP_ERRBUF_SIZE];
 
 	if (pcap_findalldevs(&alldevs, errbuf) == -1)
@@ -15,6 +18,7 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
+	/* Print the list */
 	for(d= alldevs; d != NULL; d= d->next)
 	{
 		printf("%d. %s", ++i, d->name);
@@ -23,6 +27,73 @@ int main(int argc, char *argv[])
 		else
 			printf(" (No description available)\n");
 	}
-	
+
+	if (i == 0)
+	{
+		printf("\nNo interfaces found! Make sure Npcap is installed.\n");
+		return 1;
+	}
+
+	printf("Select an interface(1-%d): ",i);
+	scanf("%d",&inum);
+
+	/* We don't need any more the device list. Free it */
+	pcap_freealldevs(alldevs);
+
 	return(0);
+}
+
+/* Print all the available information on the given interface */
+void ifprint(pcap_if_t *d)
+{
+  pcap_addr_t *a;
+  char ip6str[128];
+
+  /* Name */
+  printf("%s\n",d->name);
+
+  /* Description */
+  if (d->description)
+    printf("\tDescription: %s\n",d->description);
+
+  /* Loopback Address*/
+  printf("\tLoopback: %s\n",(d->flags & PCAP_IF_LOOPBACK)?"yes":"no");
+
+  /* IP addresses */
+  for(a=d->addresses;a;a=a->next) {
+    printf("\tAddress Family: #%d\n",a->addr->sa_family);
+  
+    switch(a->addr->sa_family)
+    {
+      case AF_INET:
+        printf("\tAddress Family Name: AF_INET\n");
+        if (a->addr)
+          printf("\tAddress: %s\n",iptos(((struct sockaddr_in *)a->addr)->sin_addr.s_addr));
+        if (a->netmask)
+          printf("\tNetmask: %s\n",iptos(((struct sockaddr_in *)a->netmask)->sin_addr.s_addr));
+        if (a->broadaddr)
+          printf("\tBroadcast Address: %s\n",iptos(((struct sockaddr_in *)a->broadaddr)->sin_addr.s_addr));
+        if (a->dstaddr)
+          printf("\tDestination Address: %s\n",iptos(((struct sockaddr_in *)a->dstaddr)->sin_addr.s_addr));
+        break;
+
+	  case AF_INET6:
+        printf("\tAddress Family Name: AF_INET6\n");
+        if (a->addr)
+          printf("\tAddress: %s\n", ip6tos(a->addr, ip6str, sizeof(ip6str)));
+       break;
+
+	  default:
+        printf("\tAddress Family Name: Unknown\n");
+        break;
+    }
+  }
+  printf("\n");
+}
+
+char * iptos(ULONG addr)
+{
+	char* address;
+	fprinf(address,"%u",addr)
+	return address;
 }
